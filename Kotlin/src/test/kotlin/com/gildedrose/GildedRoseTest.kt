412ef8b4f0f058664357bb7ed67e18526a61b987
wdt5744 <william.day@springernature.com>
2022-03-04 15:11:25 +0000

    [WD] Add multiple basic tests for non specific item

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -4,12 +4,44 @@ import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    @Test
+    fun `Normal item should degrade in sell in days remaining after a daily update`() {
+        val items = arrayOf(Item("Morning Glory Dew", 10, 10))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("foo", app.items[0].name)
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(9, app.items[0].sellIn)
+    }
 
+    @Test
+    fun `Normal item should degrade in quality after a daily update`() {
+        val items = arrayOf(Item("Morning Glory Dew", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(9, app.items[0].quality)
+    }
+
+    @Test
+    fun `Normal item should degrade in quality after multiple daily updates`() {
+        val items = arrayOf(Item("Morning Glory Dew", 10, 10))
+        val app = GildedRose(items)
+        for (days in 1..5) {
+            app.updateQuality()
+        }
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(5, app.items[0].quality)
+    }
+
+    @Test
+    fun `Normal item should degrade in sell in days after multiple daily updates`() {
+        val items = arrayOf(Item("Morning Glory Dew", 10, 10))
+        val app = GildedRose(items)
+        for (days in 1..5) {
+            app.updateQuality()
+        }
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(5, app.items[0].sellIn)
     }
 
 
