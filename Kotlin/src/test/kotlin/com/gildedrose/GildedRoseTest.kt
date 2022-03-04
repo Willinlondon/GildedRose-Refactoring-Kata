05c3b74ba1310666a8576b8668b2764260509934
wdt5744 <william.day@springernature.com>
2022-03-04 15:21:23 +0000

    [WD] Add all tests for normal item

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -44,6 +44,27 @@ class GildedRoseTest {
         assertEquals(5, app.items[0].sellIn)
     }
 
+    @Test
+    fun `Normal item should not have negative quality after multiple days`() {
+        val items = arrayOf(Item("Morning Glory Dew", 10, 2))
+        val app = GildedRose(items)
+        for (days in 1..5) {
+            app.updateQuality()
+        }
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun `Normal item quality decreases twice as fast after sell in date`() {
+        val items = arrayOf(Item("Morning Glory Dew", 2, 20))
+        val app = GildedRose(items)
+        for (days in 1..5) {
+            app.updateQuality()
+        }
+        assertEquals("Morning Glory Dew", app.items[0].name)
+        assertEquals(12, app.items[0].quality)
+    }
 
 }
 
