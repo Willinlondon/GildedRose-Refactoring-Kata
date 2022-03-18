8410e9337e4de50fa180a6f5a41729dda16dd014
wdt5744 <william.day@springernature.com>
2022-03-18 12:02:38 +0000

    [WD] Replace equals condition statements with binary operators

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,54 +6,57 @@ class GildedRose(var inventory: Array<Item>) {
         const val AGED_BRIE = "Aged Brie"
         const val SULFURAS = "Sulfuras, Hand of Ragnaros"
         const val TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+        const val MAX_QUALITY = 50
+        const val MIN_QUALITY = 0
+        const val DAILY_QUALITY_CHANGE = 1
     }
 
     fun updateQuality() {
         for (item in inventory) {
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(TICKETS)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1
+            if (item.name != AGED_BRIE && item.name != TICKETS) {
+                if (item.quality > MIN_QUALITY) {
+                    if (item.name != SULFURAS) {
+                        item.quality = item.quality - DAILY_QUALITY_CHANGE
                     }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                if (item.quality < MAX_QUALITY) {
+                    item.quality = item.quality + DAILY_QUALITY_CHANGE
 
-                    if (item.name.equals(TICKETS)) {
+                    if (item.name == TICKETS) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + DAILY_QUALITY_CHANGE
                             }
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + DAILY_QUALITY_CHANGE
                             }
                         }
                     }
                 }
             }
 
-            if (!item.name.equals(SULFURAS)) {
+            if (item.name != SULFURAS) {
                 item.sellIn = item.sellIn - 1
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(TICKETS)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1
+                if (item.name != AGED_BRIE) {
+                    if (item.name != TICKETS) {
+                        if (item.quality > MIN_QUALITY) {
+                            if (item.name != SULFURAS) {
+                                item.quality = item.quality - DAILY_QUALITY_CHANGE
                             }
                         }
                     } else {
                         item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                    if (item.quality < MAX_QUALITY) {
+                        item.quality = item.quality + DAILY_QUALITY_CHANGE
                     }
                 }
             }
